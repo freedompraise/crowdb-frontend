321d27554af974a94f4ebbe1ae222fa58c4acd4d
Praise Freedom Dike <dikepraise119@gmail.com>
2024-04-27 13:28:12 +0100

    Fix missing import in App.jsx

--- crowdb-frontend/src/App.jsx
+++ crowdb-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import configureFakeBackend from './common/api/fake-backend'
 import { Toaster } from 'sonner'
 import '@/assets/scss/app.scss'
 import '@/assets/scss/icons.scss'
+
 function App() {
 	configureFakeBackend()
 	return (
