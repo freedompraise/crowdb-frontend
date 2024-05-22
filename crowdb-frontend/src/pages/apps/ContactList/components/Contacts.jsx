5adf6d5a972f4cc1fcf97efcf5dc4c2548552612
Praise Freedom Dike <dikepraise119@gmail.com>
2024-05-22 10:28:09 +0100

    refactor: Update Contacts component with searchable table and improved UI

--- crowdb-frontend/src/pages/apps/ContactList/components/Contacts.jsx
+++ crowdb-frontend/src/pages/apps/ContactList/components/Contacts.jsx
@@ -1,48 +1,58 @@
-import { Col, Row, Card, CardBody, Image } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { ContactsData } from '../data'
+import { Table, Card, Dropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { ContactsData } from '../data';
+import { SearchBar } from '@/layout/TopNavbar/components';
 
 const Contacts = () => {
-	return (
-		<Card>
-			{ContactsData.map((contacts, idx) => {
-				return (
-					<Row lg={2} key={idx}>
-						<Card>
-							<CardBody>
-								<Col>
-									<Row>
-										<div className="media">
-											<Image
-												src={contacts.contactUserImg}
-												alt="user"
-												className="rounded-circle thumb-lg align-self-center"
-											/>
-											<div className="media-body ms-3 align-self-center">
-												<h5 className="mt-0 mb-1">{contacts.contactName}</h5>
-												<p className="mb-0 text-muted">{contacts.jobProfile}</p>
-											</div>
-										</div>
-									</Row>
-									<div className="col-auto align-self-center">
-										<ul className="list-inline mb-0">
-											<li className="list-inline-item">
-												<Link to="#" className="me-1 contact-icon">
-													<i className="fas fa-phone"></i>
-												</Link>
-												<Link to="#" className="contact-icon">
-													<i className="far fa-user"></i>
-												</Link>
-											</li>
-										</ul>
-									</div>
-								</Col>
-							</CardBody>
-						</Card>
-					</Row>
-				)
-			})}
-		</Card>
-	)
-}
-export default Contacts
+  return (
+    <Card>
+      <SearchBar />
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Customer ID</th>
+            <th>Name</th>
+            <th>Location</th>
+            <th>Status</th>
+            <th>Amount Invested</th>
+			<th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ContactsData.map((contact, idx) => (
+            <tr key={idx}>
+              <td>{contact.contactID}</td>
+              <td>
+                <Link to="#">{contact.contactName}</Link>
+              </td>
+              <td>{contact.userLocation}</td>
+              <td>
+				{contact.status === 'active'
+				? ( <span className='text-success'>{contact.status}</span>) 
+				:
+				( <span className='text-danger'>{contact.status}</span>
+				)}
+			  </td>
+              <td>{contact.amountIvested}</td>
+			  <td>
+ 				 <Dropdown>
+    				<Dropdown.Toggle variant="light" id="dropdown-basic">
+    						</Dropdown.Toggle>
+    							<Dropdown.Menu>
+      								<Dropdown.Item href="#">View Details</Dropdown.Item>
+      								<Dropdown.Item href="#">Edit Profile</Dropdown.Item>
+      								<Dropdown.Item href="#">Deactivate</Dropdown.Item>
+    					</Dropdown.Menu>
+  					</Dropdown>
+				</td>
+
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Card>
+  );
+};
+
+export default Contacts;
+
