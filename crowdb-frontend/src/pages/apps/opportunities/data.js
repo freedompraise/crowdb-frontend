b66eb7f22e69d62615385ccd54f938473ee70829
Praise Freedom Dike <dikepraise119@gmail.com>
2024-05-27 04:24:11 +0100

    refactor: Remove unused property data files and update data fetching in PropertyDetail component

---
+++ crowdb-frontend/src/pages/apps/opportunities/data.js
@@ -0,0 +1,15 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const fetchAllProperties = async () => {
+	try {
+		const properties = await fetch(`${API_URL}/package`);
+		if (!properties.ok) {
+			throw new Error('Error fetching properties');
+		}
+		const data = await properties.json();
+		return data.items;
+
+	} catch (error) {
+		console.error('Error:', error.message);
+	}
+}
