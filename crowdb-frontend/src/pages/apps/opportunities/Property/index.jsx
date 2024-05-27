2b4f9bf6182973384efc08c45a7546e24d36189e
Praise Freedom Dike <dikepraise119@gmail.com>
2024-05-27 04:24:56 +0100

    refactor: Update Property component to fetch all properties dynamically

--- crowdb-frontend/src/pages/apps/opportunities/Property/index.jsx
+++ crowdb-frontend/src/pages/apps/opportunities/Property/index.jsx
@@ -1,20 +1,20 @@
 import { PageBreadcrumb2 } from '@/components'
 import { Col, Row } from 'react-bootstrap'
-import { allProperties } from './data'
 import PropertyCard from './components/PropertyCard'
+import { fetchAllProperties } from '../data'
 
 const Property = () => {
 	return (
 		<>
 			<PageBreadcrumb2 appName="Opportunities" title="Properties" />
 			<Row>
-				{allProperties.slice(0, 4).map((property, idx) => (
+				{fetchAllProperties.slice(0, 4).map((property, idx) => (
 					<Col md={3} key={idx}>
 						<PropertyCard property={property} />
 					</Col>
 				))}
 			</Row>
-			<Row>
+			{/* <Row>
 				{allProperties.slice(4, 8).map((property, idx) => {
 					return (
 						<Col md={3} key={idx}>
@@ -22,7 +22,7 @@ const Property = () => {
 						</Col>
 					)
 				})}
-			</Row>
+			</Row> */}
 		</>
 	)
 }
