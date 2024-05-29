66c8606ab174940c6687ba7a33d2c7ef943ff82c
Praise Freedom Dike <dikepraise119@gmail.com>
2024-05-29 07:39:37 +0100

    refactor: Update login API endpoint and improve error handling

--- crowdb-frontend/src/pages/authentication/Login/useLogin.js
+++ crowdb-frontend/src/pages/authentication/Login/useLogin.js
@@ -7,59 +7,64 @@ import { useNavigate, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import * as yup from 'yup'
 
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function useLogin() {
-	const [loading, setLoading] = useState(false)
-	const navigate = useNavigate()
-	const [searchParams] = useSearchParams()
-	const { isAuthenticated, saveSession } = useAuthContext()
-	const schemaResolver = yup.object().shape({
-		email: yup
-			.string()
-			.email('Please enter a valid email')
-			.required('Please enter email'),
-		password: yup.string().required('Please enter Password'),
-	})
-	const { control, handleSubmit } = useForm({
-		resolver: yupResolver(schemaResolver),
-		defaultValues: {
-			email: 'johndoe@gmail.com',
-			password: 'password',
-		},
-	})
-	// const redirectUrl = useMemo(() => (location.state?.from.pathname, location.pathname ?? "/"), [location.state]);
-	const redirectUrl = searchParams.get('next') ?? '/dashboards/analytics'
-	const login = handleSubmit(async function (values) {
-		setLoading(true)
-		try {
-			const res = await HttpClient.post('/login', values)
-			if (res.data.token) {
-				saveSession({
-					...(res.data ?? {}),
-					token: res.data.token,
-				})
-				toast.success('Successfully logged in. Redirecting....', {
-					position: 'top-right',
-					duration: 2000,
-				})
-				navigate(redirectUrl)
-			}
-		} catch (e) {
-			if (e.response?.data?.error) {
-				toast.error(e.response?.data?.error, {
-					position: 'top-right',
-					duration: 2000,
-				})
-			}
-		} finally {
-			setLoading(false)
-		}
-	})
-	return {
-		loading,
-		login,
-		redirectUrl,
-		isAuthenticated,
-		control,
-	}
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
+  const { isAuthenticated, saveSession } = useAuthContext()
+  
+  const schemaResolver = yup.object().shape({
+    email: yup
+      .string()
+      .email('Please enter a valid email')
+      .required('Please enter email'),
+    password: yup.string().required('Please enter Password'),
+  })
+
+  const { control, handleSubmit } = useForm({
+    resolver: yupResolver(schemaResolver),
+    defaultValues: {
+      email: 'johndoe@gmail.com',
+      password: 'password',
+    },
+  })
+
+  const redirectUrl = searchParams.get('next') ?? '/apps/opportunities/property-list'
+
+  const login = handleSubmit(async function (values) {
+    setLoading(true)
+    try {
+      const res = await HttpClient.post(`${API_URL}/admins/auth/login`, values)
+      if (res.data.token) {
+        saveSession({
+          ...(res.data.user ?? {}),
+          token: res.data.token,
+        })
+        toast.success('Successfully logged in. Redirecting....', {
+          position: 'top-right',
+          duration: 2000,
+        })
+        navigate(redirectUrl)
+      }
+    } catch (e) {
+      if (e.response?.data?.message) {
+        toast.error("An unexpected error occured", {
+          position: 'top-right',
+          duration: 2000,
+        })
+      }
+    } finally {
+      setLoading(false)
+    }
+  })
+
+  return {
+    loading,
+    login,
+    redirectUrl,
+    isAuthenticated,
+    control,
+  }
 }
