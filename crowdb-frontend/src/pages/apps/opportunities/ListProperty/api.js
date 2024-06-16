3035710332e94b4f43978ac191e68fcfec431d09
Praise Freedom Dike <dikepraise119@gmail.com>
2024-06-16 03:49:53 +0100

    refactor: Update createProperty function to include authorization header in fetch request

--- crowdb-frontend/src/pages/apps/opportunities/ListProperty/api.js
+++ crowdb-frontend/src/pages/apps/opportunities/ListProperty/api.js
@@ -1,39 +1,39 @@
-
 // api.js
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL
 
 export const createProperty = async (propertyData) => {
-    try {
-      const response = await fetch(`${API_URL}/package`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(propertyData),
-      });
-  
-      if (!response.ok) {
-        throw new Error("Error sending data to the server");
-      }
-  
-      const data = await response.json();
-      return data; 
-    } catch (error) {
-      console.error("Error:", error.message);
-    }
-  };
-  
-  export const defaultFormData = {
-        name: '',
-        images: [],
-        description: '',
-        slots: 0,
-        price: 0,
-        currency: 'NGN',
-        size: '',
-        zipcode: '',
-        address: '',
-        owner: '',
-        voteOptions: ['Rental', 'Sale', 'AirBnB', 'other'],
-        amenities: [],
-      }
+	try {
+		const response = await fetch(`${API_URL}/package`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + localStorage.getItem('token'),
+			},
+			body: JSON.stringify(propertyData),
+		})
+
+		if (!response.ok) {
+			throw new Error('Error sending data to the server')
+		}
+
+		const data = await response.json()
+		return data
+	} catch (error) {
+		console.error('Error:', error.message)
+	}
+}
+
+export const defaultFormData = {
+	name: '',
+	images: [],
+	description: '',
+	slots: 0,
+	price: 0,
+	currency: 'NGN',
+	size: '',
+	zipcode: '',
+	address: '',
+	owner: '',
+	voteOptions: ['Rental', 'Sale', 'AirBnB', 'other'],
+	amenities: [],
+}
