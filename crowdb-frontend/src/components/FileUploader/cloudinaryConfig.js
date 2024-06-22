3950dfe7c654fa7113541eb5816d2bb708551dac
Praise Freedom Dike <dikepraise119@gmail.com>
2024-06-22 22:34:40 +0100

    refactor: Update cloudinaryConfig.js to return secure_url when uploading image

--- crowdb-frontend/src/components/FileUploader/cloudinaryConfig.js
+++ crowdb-frontend/src/components/FileUploader/cloudinaryConfig.js
@@ -1,12 +1,3 @@
-import { v2 as cloudinary } from 'cloudinary'
-;(async function (cloudinary) {
-	cloudinary.config({
-		cloud_name: 'dkynj442k',
-		api_key: '763167989451863',
-		api_secret: import.meta.env.VITE_CLOUDINARY_SECRET,
-	})
-})(cloudinary)
-
 const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dkynj442k/image/upload'
 const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
 
@@ -26,7 +17,7 @@ export const uploadImageToCloudinary = async (image) => {
 		}
 
 		const data = await response.json()
-		return data
+		return data.secure_url
 	} catch (error) {
 		console.error('Error uploading image:', error)
 		throw error
