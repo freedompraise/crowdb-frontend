f6225c592be9fa400c90182b9527711d249ea8d3
Praise Freedom Dike <dikepraise119@gmail.com>
2024-07-09 19:52:17 +0100

    update removeSession functionality

--- crowdb-frontend/src/context/useAuthContext.jsx
+++ crowdb-frontend/src/context/useAuthContext.jsx
@@ -1,7 +1,8 @@
 import { deleteCookie, hasCookie, setCookie } from 'cookies-next'
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 
 const AuthContext = createContext(undefined)
+
 export function useAuthContext() {
 	const context = useContext(AuthContext)
 	if (context === undefined) {
@@ -11,23 +12,40 @@ export function useAuthContext() {
 }
 
 const authTokenKey = 'CROWDB_AUTH_TOKEN'
+
 export function AuthProvider({ children }) {
-	const [user, setUser] = useState(undefined)
+	const [user, setUser] = useState(() => {
+		if (hasCookie(authTokenKey)) {
+			const cookieUser = JSON.parse(localStorage.getItem('token'))
+			return cookieUser ? { token: cookieUser } : undefined
+		}
+		return undefined
+	})
+
+	useEffect(() => {
+		if (hasCookie(authTokenKey)) {
+			const cookieUser = JSON.parse(localStorage.getItem('token'))
+			setUser(cookieUser)
+		}
+	}, [])
+
 	const saveSession = (user) => {
 		setCookie(authTokenKey, JSON.stringify(user))
 		localStorage.setItem('token', user.token)
 		setUser(user)
 	}
+
 	const removeSession = () => {
 		deleteCookie(authTokenKey)
-		setUser(undefined)
 		localStorage.removeItem('token')
+		setUser(undefined)
 	}
+
 	return (
 		<AuthContext.Provider
 			value={{
 				user,
-				isAuthenticated: hasCookie(authTokenKey),
+				isAuthenticated: !!user,
 				saveSession,
 				removeSession,
 			}}>
