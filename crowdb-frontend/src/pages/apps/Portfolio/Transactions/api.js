19df3e37eb9fcf6173f372f83b4b7fb46abe4519
Praise Freedom Dike <dikepraise119@gmail.com>
2024-07-28 13:31:36 +0100

    refactor: Update API endpoint for fetching portfolio transactions

--- crowdb-frontend/src/pages/apps/Portfolio/Transactions/api.js
+++ crowdb-frontend/src/pages/apps/Portfolio/Transactions/api.js
@@ -4,12 +4,9 @@ const API_URL = import.meta.env.VITE_API_URL
 
 export const fetchPortfolioTransactions = async () => {
 	try {
-		const response = await axios.get(
-			`${API_URL}/admin/portfolio/transactions`,
-			{
-				headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-			}
-		)
+		const response = await axios.get(`${API_URL}/admin/portfolio`, {
+			headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
+		})
 
 		return response.data.data
 	} catch (err) {
