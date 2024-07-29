405289425ac158c7522528062a632125d19092a4
Praise Freedom Dike <dikepraise119@gmail.com>
2024-07-29 03:17:50 +0100

    refactor: Improve UI structure and add Card component to VotesHistory component

--- crowdb-frontend/src/pages/apps/opportunities/PropertyDetail/components/VotesHistory.jsx
+++ crowdb-frontend/src/pages/apps/opportunities/PropertyDetail/components/VotesHistory.jsx
@@ -1,5 +1,13 @@
 import { useState, useEffect } from 'react'
-import { Modal, Button, Table, Alert, Spinner, Card } from 'react-bootstrap'
+import {
+	Modal,
+	Button,
+	Table,
+	Alert,
+	Spinner,
+	Card,
+	CardBody,
+} from 'react-bootstrap'
 import axios from 'axios'
 
 const VotesHistory = ({ propertyId }) => {
@@ -68,74 +76,81 @@ const VotesHistory = ({ propertyId }) => {
 
 	return (
 		<Card className="mb-3">
+			<h5 className="px-3">Votes History</h5>
 			{loading ? (
 				<Spinner animation="border" />
 			) : error ? (
 				<Alert variant="danger">{error}</Alert>
 			) : sessions.length === 0 ? (
-				<Alert variant="info">No votes on this property yet</Alert>
+				<Alert className="mx-3" variant="info">
+					No votes on this property yet
+				</Alert>
 			) : (
-				<Button onClick={handleShowModal}>Show Votes History</Button>
+				<Button className="mx-3" onClick={handleShowModal}>
+					Show Votes History
+				</Button>
 			)}
+			<CardBody className="">
+				<Modal show={showModal} onHide={handleCloseModal}>
+					<Modal.Header closeButton>
+						<Modal.Title>Votes History</Modal.Title>
+					</Modal.Header>
 
-			<Modal show={showModal} onHide={handleCloseModal}>
-				<Modal.Header closeButton>
-					<Modal.Title>Votes History</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					{sessions.map((session) => (
-						<div key={session.id} className="mb-3">
-							<h5>Session ID: {session.id}</h5>
-							<Table bordered hover>
-								<thead>
-									<tr>
-										<th>ID</th>
-										<th>Created At</th>
-										<th>Option</th>
-										<th>Weight</th>
-										<th>User</th>
-									</tr>
-								</thead>
-								<tbody>
-									{votes[session.id] &&
+					<Modal.Body>
+						{sessions.map((session) => (
+							<div key={session.id} className="mb-3">
+								<h5>Session ID: {session.id}</h5>
+								<Table bordered hover>
+									<thead>
+										<tr>
+											<th>ID</th>
+											<th>Created At</th>
+											<th>Option</th>
+											<th>Weight</th>
+											<th>User</th>
+										</tr>
+									</thead>
+									<tbody>
+										{votes[session.id] &&
+										votes[session.id].data &&
+										Array.isArray(votes[session.id].data) &&
+										votes[session.id].data.length > 0 ? (
+											votes[session.id].data.map((vote) => (
+												<tr key={vote.id}>
+													<td>{vote.id}</td>
+													<td>{new Date(vote.createdAt).toLocaleString()}</td>
+													<td>{vote.option}</td>
+													<td>{vote.weight}</td>
+													<td>{`${vote.user.firstName} ${vote.user.lastName}`}</td>
+												</tr>
+											))
+										) : (
+											<tr>
+												<td colSpan="6" className="text-center">
+													No votes for this session
+												</td>
+											</tr>
+										)}
+									</tbody>
+								</Table>
+								{votes[session.id] &&
 									votes[session.id].data &&
 									Array.isArray(votes[session.id].data) &&
-									votes[session.id].data.length > 0 ? (
-										votes[session.id].data.map((vote) => (
-											<tr key={vote.id}>
-												<td>{vote.id}</td>
-												<td>{new Date(vote.createdAt).toLocaleString()}</td>
-												<td>{vote.option}</td>
-												<td>{vote.weight}</td>
-												<td>{`${vote.user.firstName} ${vote.user.lastName}`}</td>
-											</tr>
-										))
-									) : (
-										<tr>
-											<td colSpan="6" className="text-center">
-												No votes for this session
-											</td>
-										</tr>
+									votes[session.id].data.length === 0 && (
+										<Alert variant="info" className="mt-2">
+											No votes for this session
+										</Alert>
 									)}
-								</tbody>
-							</Table>
-							{votes[session.id] &&
-								votes[session.id].data &&
-								Array.isArray(votes[session.id].data) &&
-								votes[session.id].data.length === 0 && (
-									<Alert variant="info" className="mt-2">
-										No votes for this session
-									</Alert>
-								)}
-						</div>
-					))}
-				</Modal.Body>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={handleCloseModal}>
-						Close
-					</Button>
-				</Modal.Footer>
-			</Modal>
+							</div>
+						))}
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={handleCloseModal}>
+							Close
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			</CardBody>
 		</Card>
 	)
 }
